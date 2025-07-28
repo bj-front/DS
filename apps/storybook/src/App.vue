d7e40c04d595728a2993a8fc0a86b1087e3f47df
Benoit Jolly <benoit.jolly@club-employes.com>
2025-07-28 15:43:59 +0200

    Refactor: Remove unused components and assets from Storybook and Utopia Design System

--- apps/storybook/src/App.vue
+++ apps/storybook/src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// Clean Vue 3 app for Storybook
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Utopia Design System</h1>
+    <p>Welcome to your clean Design System setup</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1 {
+  color: #42b883;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin: 2rem 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p {
+  text-align: center;
+  color: #666;
 }
 </style>
